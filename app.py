2b4af04f41441ce474b646ddb1f93d61d01d75d2
leeminze214 <leeminz214@gmail.com>
2020-09-27 00:42:12 -0400

    database, queries , routes, backend complete

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 
-
+from db_methods import methods, config
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'asdkjhl2938h82h43ph9f0h8(*&go9d8fg'
-io = SocketIO(app)
+app.config['SECRET_KEY'] = 'asdkjhlsdfg'
+execute = methods(config)
 
-@app.route('/login', methods=["GET","POST"])
+@app.route('/', methods=["GET","POST"])
 def login():
     form = request.form.to_dict()
-    session['first'] = form['first']
-    session['last'] = form['last']
-    session['address'] = form['address']
-    session['provider'] = form['provider']
-    session['spot'] = 'none'#getting max from db
-    render_template('login.html')
+    if 'first' in form and 'last' in form and 'birthday' in form and 'provider' in form:
+        session['first'] = first = form['first']
+        session['last'] = last = form['last']
+        session['birthday'] = birthday = form['birthday']
+        session['provider'] = provider = form['provider']
+        session['spot'] = spot = execute.new_spot()
+        execute.new_entry(first, last, birthday, provider, spot)
+        return redirect('/queues')
+    return render_template('login.html')
 
 @app.route('/queues')
 def queues():
-    session['spot'] = 'spot'#getting users new spot
-    render_template('queues.html', name = session['first'], spot = session['spot'])
+    first = session['first']
+    last = session['last']
+    tem_spot = execute.get_spot(first, last)
+    session['spot'] = tem_spot[0]
+    infront = execute.total_infront(first, last)
+    return render_template('queues.html', name = session['first'], spot = session['spot'], total_infront = infront)
 
 @app.route('/leave')
 def leave():
-    #delete user from db
-    pass
+    first = session['first']
+    last = session['last']
+    spot = session['spot']
+    execute.leave(first, last, spot)
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug = True)
+
+#connect heroku database
